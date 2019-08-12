d8d5d9a8dbd7d2d6821951bf481733429869bb89
shichang hou <houshichang@hotmail.com>
2019-08-12 22:52:45 +1000

    added unit tests

---
+++ BizCover.Api.Cars.UnitTests/Domains/Discount/NumberOfCarsDiscountTests.cs
@@ -0,0 +1,90 @@
+﻿using BizCover.Api.Cars.AppSettings;
+using BizCover.Api.Cars.Domains;
+using BizCover.Api.Cars.Domains.Discount;
+using Microsoft.Extensions.Options;
+using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace BizCover.Api.Cars.UnitTests.Domains.Discount
+{
+    public class NumberOfCarsDiscountTests
+    {
+        private Mock<IOptions<DiscountSetting>> _mockDiscountSetting;
+        private NumberOfCarsDiscount _numberOfCarsDiscount;
+
+        public NumberOfCarsDiscountTests()
+        {
+            _mockDiscountSetting = new Mock<IOptions<DiscountSetting>>();
+
+            var _discountSettingStub = new DiscountSetting
+            {
+                EligibleYear = 2000,
+                EligibleYearDiscount = 0.5m,
+                EligibleYearDiscountApplyOrder = 1,
+                EligibleNumberOfCars = 2,
+                EligibleNumberOfCarsDiscount = 0.5m,
+                EligibleNumberOfCarsDiscountApplyOrder = 2,
+                EligibleTotalCost = 1000,
+                EligibleTotalCostDiscount = 0.5m,
+                EligibleTotalCostDiscountApplyOrder = 3,
+            };
+
+            _mockDiscountSetting.Setup(q => q.Value).Returns(_discountSettingStub);
+
+            _numberOfCarsDiscount = new NumberOfCarsDiscount(_mockDiscountSetting.Object);
+        }
+
+        [Fact]
+        public void given_two_cars_and_no_discounted_total_cost_should_not_have_discount()
+        {
+            // setup
+            var givenCars = new List<CarDomain>
+            {
+                new CarDomain(1, "silver", "japan", "subaru", "outback", 10m, 2009),
+                new CarDomain(1, "silver", "japan", "subaru", "outback", 10m, 2009)
+            };
+
+            // act
+            var result = _numberOfCarsDiscount.Apply(givenCars, null);
+
+            // assert
+            Assert.Equal(20m, result);
+        }
+
+        [Fact]
+        public void given_two_cars_and_discounted_total_cost_should_not_have_discount()
+        {
+            // setup
+            var givenCars = new List<CarDomain>
+            {
+                new CarDomain(1, "silver", "japan", "subaru", "outback", 10m, 2009),
+                new CarDomain(1, "silver", "japan", "subaru", "outback", 10m, 2009)
+            };
+
+            // act
+            var result = _numberOfCarsDiscount.Apply(givenCars, 15m);
+
+            // assert
+            Assert.Equal(15m, result);
+        }
+
+        [Fact]
+        public void given_three_cars_and_discounted_total_cost_should_have_discount()
+        {
+            // setup
+            var givenCars = new List<CarDomain>
+            {
+                new CarDomain(1, "silver", "japan", "subaru", "outback", 10m, 2009),
+                new CarDomain(1, "silver", "japan", "subaru", "outback", 10m, 2009),
+                new CarDomain(1, "silver", "japan", "subaru", "outback", 10m, 2009)
+            };
+
+            // act
+            var result = _numberOfCarsDiscount.Apply(givenCars, null);
+
+            // assert
+            Assert.Equal(15m, result);
+        }
+    }
+}
---
+++ BizCover.Api.Cars.UnitTests/Services/CarsServiceTests.cs
@@ -0,0 +1,106 @@
+﻿using BizCover.Api.Cars.Domains;
+using BizCover.Api.Cars.Repository;
+using BizCover.Api.Cars.Repository.Dtos;
+using BizCover.Api.Cars.Services;
+using Moq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BizCover.Api.Cars.UnitTests.Services
+{
+    public class CarsServiceTests
+    {
+        private Mock<ICarsRepository> _mockCarsRepository;
+        private CarsService _carsService;
+
+        public CarsServiceTests()
+        {
+            _mockCarsRepository = new Mock<ICarsRepository>();
+            _carsService = new CarsService(_mockCarsRepository.Object);
+        }
+
+        [Fact]
+        public async Task get_all_api_should_retrieve_all_cars_information_from_repository()
+        {
+            // setup
+            _mockCarsRepository.Setup(c => c.GetAll()).Verifiable();
+
+            // act
+            await _carsService.GetAll();
+
+            // assert
+            _mockCarsRepository.Verify(c => c.GetAll(), Times.Once);
+        }
+
+        [Fact]
+        public async Task given_id_get_api_should_retrieve_a_car_information_from_repository()
+        {
+            // setup
+            _mockCarsRepository.Setup(c => c.Get(It.IsAny<int>())).Verifiable();
+
+            // act
+            await _carsService.Get(1);
+
+            // assert
+            _mockCarsRepository.Verify(c => c.Get(1), Times.Once);
+        }
+
+        [Fact]
+        public async Task given_car_domain_obj_update_api_should_update_repository()
+        {
+            // setup
+            var givenCarDomain = new CarDomain(1, "blue", "japan", "subaru", "outback", 10000m, 2009);
+            var mockGetResult = new CarDomain(1, "silver", "japan", "subaru", "outback", 10000m, 2009);
+            var mockRepositoryUpdateResult = new CarsRepositoryResultDto { IsSuccess = true };
+            _mockCarsRepository.Setup(c => c.Get(It.IsAny<int>())).ReturnsAsync(mockGetResult);
+            _mockCarsRepository.Setup(c => c.Update(It.IsAny<CarDomain>())).ReturnsAsync(mockRepositoryUpdateResult);
+
+            // act
+            var result = await _carsService.Update(givenCarDomain);
+
+            // assert
+            _mockCarsRepository.Verify(c => c.Get(It.IsAny<int>()), Times.Once);
+            _mockCarsRepository.Verify(c => c.Update(It.IsAny<CarDomain>()), Times.Once);
+            Assert.Equal(result.IsSuccess, mockRepositoryUpdateResult.IsSuccess);
+            Assert.Equal(result.Message, mockRepositoryUpdateResult.Message);
+        }
+
+        [Fact]
+        public async Task given_car_domain_obj_update_api_throws_ex_should_return_correct_response()
+        {
+            // setup
+            var givenCarDomain = new CarDomain(1, "blue", "japan", "subaru", "outback", 10000m, 2009);
+            var mockGetResult = new CarDomain(1, "silver", "japan", "subaru", "outback", 10000m, 2009);
+            _mockCarsRepository.Setup(c => c.Get(It.IsAny<int>())).ReturnsAsync(mockGetResult);
+            _mockCarsRepository.Setup(c => c.Update(It.IsAny<CarDomain>())).ThrowsAsync(new System.Exception());
+
+            // act
+            var result = await _carsService.Update(givenCarDomain);
+
+            // assert
+            _mockCarsRepository.Verify(c => c.Get(It.IsAny<int>()), Times.Once);
+            _mockCarsRepository.Verify(c => c.Update(It.IsAny<CarDomain>()), Times.Once);
+            Assert.False(result.IsSuccess);
+            Assert.Equal("Update operation failed", result.Message);
+        }
+
+        [Fact]
+        public async Task given_car_domain_obj_add_api_should_add_it_to_repository()
+        {
+            // setup
+            var givenCarDomain = new CarDomain(1, "blue", "japan", "subaru", "outback", 10000m, 2009);
+            _mockCarsRepository.Setup(c => c.Add(givenCarDomain)).ReturnsAsync(1);
+            _mockCarsRepository.Setup(c => c.GetAll()).ReturnsAsync(new List<CarDomain>());
+
+            // act
+            var result = await _carsService.Add(givenCarDomain);
+
+            // assert
+            _mockCarsRepository.Verify(c => c.Add(givenCarDomain), Times.Once);
+            _mockCarsRepository.Verify(c => c.GetAll(), Times.Once);
+            Assert.True(result.IsSuccess);
+            Assert.Empty(result.Message);
+        }
+    }
+}
